a84b3dc9ee22de0089f6ddf9b9194cf57605782d
zhoumaiqi <1016276875@qq.com>
2024-12-29 16:05:19 +0800

    🎈 perf: 调整qos

--- src/my_behavior_tree_project/nodes/is_game_started.cpp
+++ src/my_behavior_tree_project/nodes/is_game_started.cpp
@@ -2,6 +2,7 @@
 #include <behaviortree_cpp_v3/behavior_tree.h>
 #include <behaviortree_cpp_v3/bt_factory.h>
 
+
 IsGameStarted::IsGameStarted(const std::string& name, const BT::NodeConfiguration& config)
     : ConditionNode(name, config), game_started_(false)
 {
@@ -9,8 +10,9 @@ IsGameStarted::IsGameStarted(const std::string& name, const BT::NodeConfiguratio
     node_ = rclcpp::Node::make_shared("is_game_started_node");
 
     // 创建一个订阅者，订阅game_status话题
+    rclcpp::QoS qos_profile = rclcpp::QoS(100).reliable().transient_local();
     subscription_ = node_->create_subscription<pb_rm_interfaces::msg::GameStatus>(
-        "game_status", 10, std::bind(&IsGameStarted::gameStatusCallback, this, std::placeholders::_1));
+        "game_status", qos_profile, std::bind(&IsGameStarted::gameStatusCallback, this, std::placeholders::_1));
 }
 
 BT::PortsList IsGameStarted::providedPorts()
@@ -21,12 +23,20 @@ BT::PortsList IsGameStarted::providedPorts()
 BT::NodeStatus IsGameStarted::tick()
 {
     std::lock_guard<std::mutex> lock(mutex_);
-    RCLCPP_INFO(rclcpp::get_logger("IsGameStarted"), "Checking if game has started: %s", game_started_ ? "Yes" : "No");
+    if (game_started_)
+    {
+        RCLCPP_INFO(rclcpp::get_logger("IsGameStarted"), "Game has started.");
+    }
+    else
+    {
+        RCLCPP_INFO(rclcpp::get_logger("IsGameStarted"), "Game has not started.");
+    }
     return game_started_ ? BT::NodeStatus::SUCCESS : BT::NodeStatus::FAILURE;
 }
 
 void IsGameStarted::gameStatusCallback(const pb_rm_interfaces::msg::GameStatus::SharedPtr msg)
 {
     std::lock_guard<std::mutex> lock(mutex_);
+    RCLCPP_INFO(rclcpp::get_logger("IsGameStarted"), "Received game status message: %d", msg->game_progress);
     game_started_ = (msg->game_progress == pb_rm_interfaces::msg::GameStatus::RUNNING);
 }
